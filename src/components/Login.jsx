88f1952d53b9ed46422f17bc61e453ae31ef3752
Joan-Maina <mainajoan12@gmail.com>
2022-08-04 11:07:42 +0300

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <form action="">
+        <div className="email">
+          <input type="email" />
+        </div>
+        <div className="password">
+          <input type="password" />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
